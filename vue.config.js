a01a6f9cd64d08e70d5b76315e271639bf1e6056
wungjyan <1170017412@qq.com>
2019-06-28 10:48:05 +0800

    配置文件

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  lintOnSave: true,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  }
+}
