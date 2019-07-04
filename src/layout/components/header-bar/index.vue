2b6b64db9efa5740b1c05ae31b6da942bebf4a30
wungjyan <1170017412@qq.com>
2019-07-04 16:18:05 +0800

    添加头部组件

---
+++ src/layout/components/header-bar/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header-bar">
+    <sider-trigger :collapsed="collapsed" icon="md-menu" @on-change="handleCollpasedChange"></sider-trigger>
+  </div>
+</template>
+
+<script>
+import SiderTrigger from './sider-trigger.vue'
+export default {
+  name: 'HeaderBar',
+  components: { SiderTrigger },
+  props: {
+    collapsed: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  methods: {
+    handleCollpasedChange (state) {
+      this.$emit('on-coll-change', state)
+    }
+  },
+  mounted () { }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/layout/components/header-bar/sider-trigger.vue
@@ -0,0 +1,57 @@
+<template>
+  <div @click="handleChange" :class="['sider-trigger-div',collapsed?'collapsed':'']">
+    <Icon :type="icon" :size="size" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SiderTrigger',
+  props: {
+    collapsed: {
+      type: Boolean,
+      default: false
+    },
+    icon: {
+      type: String,
+      default: 'md-menu'
+    },
+    size: {
+      type: Number,
+      default: 26
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  methods: {
+    handleChange () {
+      this.$emit('on-change', !this.collapsed)
+    }
+  },
+  mounted () { }
+}
+</script>
+<style lang="less" scoped>
+.trans {
+  transition: transform 0.2s ease;
+}
+.sider-trigger-div {
+  padding: 6px;
+  width: 40px;
+  height: 40px;
+  text-align: center;
+  color: #5c6b77;
+  margin-top: 12px;
+  i {
+    .trans;
+    vertical-align: top;
+  }
+  &.collapsed i {
+    transform: rotateZ(90deg);
+    .trans;
+  }
+}
+</style>
