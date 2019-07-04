57bc1573306a8a8396b53e0a0b027efc1e34b1e7
wungjyan <1170017412@qq.com>
2019-07-04 14:02:23 +0800

    update

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <Layout style="height: 100%" class="app-wrapper">
-    <Sider>
+  <Layout style="height: 100%" class="layout-wrapper">
+    <Sider :width="180" hide-trigger collapsible v-model="collapsed">
       <side-menu :menu-list="menuList" theme="dark" :active-name="$route.name" @on-select="turnToPage"></side-menu>
     </Sider>
     <Layout>
-      <Header>头部</Header>
+      <Header class="header-con">头部</Header>
       <Content>
         <router-view />
       </Content>
@@ -19,6 +19,7 @@ export default {
   components: { SideMenu },
   data () {
     return {
+      collapsed: false
     }
   },
   computed: {
@@ -28,6 +29,7 @@ export default {
   },
   methods: {
     turnToPage (name) {
+      // 路由名带有href时直接跳转外部链接
       if (name.indexOf('isTurnByHref_') > -1) {
         window.open(name.split('_')[1])
         return
@@ -40,5 +42,6 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style lang="less" scoped>
+@import "./index.less";
 </style>
