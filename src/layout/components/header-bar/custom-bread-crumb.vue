00423a6944cfbc387dad7e2209a6eb18192ed406
wungjyan <1170017412@qq.com>
2019-07-12 16:10:01 +0800

    面包屑组件

---
+++ src/layout/components/header-bar/custom-bread-crumb.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="custom-bread-crumb">
+    <Breadcrumb>
+      <BreadcrumbItem to="/">首页</BreadcrumbItem>
+      <template v-if="list.length">
+        <BreadcrumbItem v-for="(item,index) in list" :key="index">{{item.title}}</BreadcrumbItem>
+      </template>
+    </Breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomBreadCrumb',
+  props: {
+    list: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  methods: {},
+  mounted () { }
+}
+</script>
+<style scoped>
+.custom-bread-crumb {
+  display: inline-block;
+  vertical-align: top;
+  margin-left: 20px;
+}
+</style>
